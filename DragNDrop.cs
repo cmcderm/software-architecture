32101b8901e99fb715df932a4be57ae63de9832e
Connor McDermott <connormcderm@protonmail.com>
2025-08-19 12:38:24 -0500

    Remove run.sh BS, got a working hello world every frame

--- DragNDrop.cs
+++ DragNDrop.cs
@@ -1,7 +1,14 @@
 using Godot;
 using System;
 
-public partial class DragNDrop : Node2D
-{
+public partial class DragNDrop : Node2D {
 	
+	public override void _Ready() {
+		GD.Print("Hello from Ready!");
+	}
+	
+	public override void _Process(double delta) {
+		GD.Print("Hello World!");
+		
+	}
 }
